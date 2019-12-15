4b4be8f714adacb1a2cf4b9615e81eaa0d564f8d
Thinesh <thineshprathaban@gmail.com>
2019-12-16 06:10:04 +0800

    Version 1

---
+++ app/src/main/java/us/to/opti_grader/optigrader/LeaderboardExamList.java
@@ -0,0 +1,13 @@
+package us.to.opti_grader.optigrader;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LeaderboardExamList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_leaderboard_exam_list);
+    }
+}
